172b8324d9c3daf1f7c3c8cdfc7e1172491adbad
spcvanschie <sebastiaan.van.schie@live.nl>
2016-03-17 15:09:06 +0100

    Small code adjustments

--- Code/annulus_calc.m
+++ Code/annulus_calc.m
@@ -1,7 +1,12 @@
-function [W,phi,AoA,Cx,Cy,a_new,a_tan_new]=annulus_calc(N,a,a_tan,U_inf,r,omega,chordangle,Clspline,Cdspline,blade_solidity)
+function [W,phi_all,AoA_all,Cx,Cy,a_new,a_tan_new]=annulus_calc(N,U_inf,r,omega,chordangle,Clspline,Cdspline,blade_solidity)
+% Initial guesses for a and a_tangential
+a = 0.5*ones(1,N);
+a_tan = 0.4*ones(1,N);
 
-a_new=zeros(1,N);
-a_tan_new=zeros(1,N);
+a_new = zeros(1,N);
+a_tan_new = zeros(1,N);
+phi_all = zeros(1,N);
+AoA_all = zeros(1,N);
 for i = (1:N)
     a_1 = a(i);
     a_tan_1 = a_tan(i);
@@ -20,8 +25,8 @@ for i = (1:N)
         torque_a_tan = blade_solidity(i).*Cy./(4.*sinphi.*cosphi);
         a_calc = thrust_a./(1+thrust_a);
         a_tan_calc = torque_a_tan./(1-torque_a_tan);
-        if abs(a_calc-a_1)<0.01.*a_calc 
-            if abs(a_tan_calc-a_tan_1)<0.01.*a_tan
+        if abs(a_calc-a_1)<0.001.*a_calc 
+            if abs(a_tan_calc-a_tan_1)<0.001.*a_tan_calc
                 run = 0;
             end
         end
@@ -30,4 +35,6 @@ for i = (1:N)
     end
     a_new(i) = a_calc;
     a_tan_new(i) = a_tan_calc;
+    phi_all(i) = phi;
+    AoA_all(i) = AoA;
 end
--- Code/main.m
+++ Code/main.m
@@ -1,6 +1,6 @@
-N = 3; % number of annuli
+N = 50; % number of annuli
 pitch = 2; % blade pitch angle [deg]
-lambda = 6; % tip speed ratio [-]
+lambda = 10; % tip speed ratio [-]
 U_inf = 10; % freestream velocity [m/s]
 
 % calculate interpolation splines for airfoil Cl and Cd
@@ -9,9 +9,12 @@ U_inf = 10; % freestream velocity [m/s]
 % calculate geometrical parameters for each annulus
 [r,R,B,mu_min,mu_local,twist,chordlength,chordangle,omega,blade_solidity]= geometry(N,pitch,lambda,U_inf);
 
-% Initial guesses for a and a_tangential
-a = 0.3*ones(1,N);
-a_tan = 0.2*ones(1,N);
-
 % calculate annulus characteristics, contains iteration loop for induction factors
-[W,phi,AoA,Cx,Cy,a_new,a_tan_new]=annulus_calc(N,a,a_tan,U_inf,r,omega,chordangle,Clspline,Cdspline,blade_solidity);
+[W,phi_all,AoA,Cx,Cy,a_new,a_tan_new]=annulus_calc(N,U_inf,r,omega,chordangle,Clspline,Cdspline,blade_solidity);
+
+figure(1)
+plot(mu_local,a_new)
+grid on
+title('Induction factor')
+xlabel('$\frac{r}{R} [-]$','Interpreter','LaTex')
+ylabel('Induction factor a [-]')
