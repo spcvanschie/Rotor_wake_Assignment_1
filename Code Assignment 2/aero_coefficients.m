1b070025ccd4104559bdc4bd9c43ef3eae40cf94
spcvanschie <sebastiaan.van.schie@live.nl>
2016-05-31 16:42:19 +0200

    Code converges and seems to function properly, now adding extra graphs and output figures

--- Code Assignment 2/aero_coefficients.m
+++ Code Assignment 2/aero_coefficients.m
@@ -1,4 +1,4 @@
-function [dL,C_L,dD_i,C_D_i,alpha_i,C_N,C_T,w_ind,u_magnitude] = aero_coefficients(rho,chordlength,surface_area,u_inflow,circulation,B_ind,alpha,Clspline,Cdspline)
+function [dL,C_L,dD_i,C_D_i,alpha_i,C_N,C_T,w_ind,u_magnitude,dQ,dQ_cumulative,C_Q,C_Q_cumulative,dT] = aero_coefficients(N,R,rho,chordlength,surface_area,chord,u_inflow,u_freestream,circulation,B_ind,alpha,Clspline,Cdspline,panel_width,cp_spanwise)
 u_magnitude = zeros(length(circulation),1);
 for i = 1:length(circulation)
     u_magnitude(i) = norm(u_inflow(:,i));
@@ -13,15 +13,24 @@ alpha_i = zeros(length(circulation),1);
 alpha_eff = zeros(length(circulation),1);
 C_N = zeros(length(circulation),1);
 C_T = zeros(length(circulation),1);
+dQ = zeros(length(circulation),1);
+dQ_cumulative = zeros(length(circulation),1);
+C_Q = zeros(length(circulation),1);
+C_Q_cumulative = zeros(length(circulation),1);
+dT = zeros(length(circulation),1);
 for i = 1:length(circulation)
     alpha_i(i) = -rad2deg((w_ind(i)/u_magnitude(i)));
     alpha_eff(i) = -alpha_i(i) - alpha(i);
-    dL(i) = rho*circulation(i)*u_magnitude(i)*(surface_area(i)/chordlength(i));
+    dL(i) = rho*circulation(i)*u_magnitude(i)*panel_width(i);
     C_L(i) = ppval(Clspline,alpha_eff(i)); %circulation(i)/(0.5*chordlength(i)*u_magnitude(i));
-    dD_i(i) = -rho*w_ind(i)*circulation(i)*(surface_area(i)/chordlength(i));
+    dD_i(i) = -rho*w_ind(i)*circulation(i)*panel_width(i);
     C_D_i(i) = ppval(Cdspline,alpha_eff(i)); %-circulation(i)/(0.5*chordlength(i)*w_ind(i));
     C_N(i) = cos(deg2rad(alpha(i)))*C_L(i) + sin(deg2rad(alpha(i)))*C_D_i(i);
     C_T(i) = -sin(deg2rad(alpha(i)))*C_L(i) + cos(deg2rad(alpha(i)))*C_D_i(i);
-
+    dQ(i) = 0.5*rho*(u_magnitude(i)^2)*N*chord(i)*cp_spanwise(i)*(dL(i)*sin(deg2rad(alpha_eff(i)))-dD_i(i)*cos(deg2rad(alpha_eff(i))))*panel_width(i);
+    dQ_cumulative(i) = sum(dQ(1:i));
+    C_Q(i) = dQ(i)/(0.5*rho*(norm(u_freestream)^2)*pi*(R^3));
+    C_Q_cumulative(i) = dQ_cumulative(i)/(0.5*rho*(norm(u_freestream)^2)*pi*(R^3));
+    dT(i) = 0.5*rho*(u_magnitude(i)^2)*N*chord(i)*(C_L(i)*cos(deg2rad(alpha_eff(i)))+C_D_i(i)*sin(deg2rad(alpha_eff(i))))*panel_width(i);
 end
 end
--- Code Assignment 2/main.m
+++ Code Assignment 2/main.m
@@ -9,14 +9,14 @@ blade_root = 0.2*span; % location of blade root [m]
 rootchord = 4; % root chord length (at mu = 0) [m]
 tipchord = 2; % tip chord length [m]
 bladepitch = -1.5; % blade pitch angle [deg]
-roottwist = 1; % root twist angle [deg]
+roottwist = 14; % root twist angle [deg]
 tiptwist = 0; % tip twist angle [deg]
 u_inf = [10;0;0]; % freestream velocity vector [m/s]
 tsr = 8; % Tip Speed Ratio [-]
 omega = tsr*u_inf(1)/span; % rotor rotational rate in rad/s
 
 %% Discretisation parameter declaration
-n = 80; % number of collocation points [-]
+n = 40; % number of collocation points [-]
 dt = 0.01; % time step [s]
 timestep = 0; % initial time step number [-]
 rotation_angle = 0; % initial azimuthal blade rotation angle [rad], defined as 0 along the y-axis
@@ -40,7 +40,9 @@ vort_end_spanwise = vort_end_y; % radial position of every vortex element endpoi
 chord = chordlength(span,rootchord,tipchord,cp_init_y);
 alpha = blade_angle(span,roottwist,tiptwist,bladepitch,cp_init_y);
 surface_area = surfarea(chord,vort_end_y);
+panel_width = surface_area./chord;
 cp_spanwise = cp_init_y; % radial position of every collocation point
+cp_mu = cp_spanwise./span;
 
 %% Adjustment of collocation point x-coordinates and trailing edge vortex corners
 cp_init_x = 0.75.*chord;
@@ -68,8 +70,9 @@ circulations = -A_ind_init\(Q_inf_init);
 
 %% Time marching section for t > 0
 
-max_timestep = 100;
+max_timestep = 200;
 
+Thrust_convergence = zeros(max_timestep,1);
 Q_ind_start = zeros(length(chord),max_timestep-1);
 %% Start of time marching
 for timestep = 1:max_timestep
@@ -107,15 +110,21 @@ for timestep = 1:max_timestep
         TE_convected = TE_convected + repmat(dt*u_inf,1,length(TE_blade(1,:)),timestep);
         circulation_history = cat(3,circulation_history,circulation_blade);
     end
-        
+    
+    B_ind = induction_factors(normalvectors_upd,cp_coords_blade,vort_end_blade,TE_blade,eps,1);
+    [dL,C_L,dD_i,C_D_i,alpha_i,C_N,C_T,w_ind,u_magnitude,dQ,dQ_cumulative,C_Q,C_Q_cumulative,dT] = aero_coefficients(N,span,rho,chord,surface_area,chord,u_rotor_upd,u_inf,circulation_blade,B_ind,alpha,Clspline,Cdspline,panel_width,cp_spanwise);
+    Thrust_convergence(timestep) = sum(dT);
+    
+    
     time = dt*(timestep-1);
     rotation_angle = omega*time; % blade rotation angle w.r.t. positive y-axis [rad]
 end
 
 %% Calculating several forces & coefficients for the now-converged flow over the rotor blade
 B_ind = induction_factors(normalvectors_upd,cp_coords_blade,vort_end_blade,TE_blade,eps,1);
-[dL,C_L,dD_i,C_D_i,alpha_i,C_N,C_T,w_ind,u_magnitude] = aero_coefficients(rho,chord,surface_area,u_rotor_upd,circulation_blade,B_ind,alpha,Clspline,Cdspline);
-
+[dL,C_L,dD_i,C_D_i,alpha_i,C_N,C_T,w_ind,u_magnitude,dQ,dQ_cumulative,C_Q,C_Q_cumulative,dT] = aero_coefficients(N,span,rho,chord,surface_area,chord,u_rotor_upd,u_inf,circulation_blade,B_ind,alpha,Clspline,Cdspline,panel_width,cp_spanwise);
+P = omega*dQ_cumulative(end);
+CP = P/(0.5*rho*(norm(u_inf)^3)*pi*span^2);
 %% Remarks on convergence:
 % 1) The circulation curve becomes smoother when less elements are used. I
 % have no idea why this is, maybe the numerical round-off errors induced by
@@ -125,49 +134,67 @@ B_ind = induction_factors(normalvectors_upd,cp_coords_blade,vort_end_blade,TE_bl
 
 %% Plotting module
 figure(1)
-grid on
-plot(cp_spanwise,circulation_history(:,1,1),cp_spanwise,circulation_history(:,1,3),cp_spanwise,circulation_history(:,1,5),cp_spanwise,circulation_history(:,1,7),cp_spanwise,circulation_history(:,1,10),cp_spanwise,circulation_history(:,1,14),cp_spanwise,circulation_history(:,1,18),cp_spanwise,circulation_history(:,1,22),cp_spanwise,circulation_history(:,1,26),cp_spanwise,circulation_history(:,1,30),cp_spanwise,circulation_history(:,1,35),cp_spanwise,circulation_history(:,1,40),cp_spanwise,circulation_history(:,1,45),cp_spanwise,circulation_history(:,1,50),cp_spanwise,circulation_history(:,1,60),cp_spanwise,circulation_history(:,1,70),cp_spanwise,circulation_history(:,1,80),cp_spanwise,circulation_history(:,1,90));%,cp_spanwise,circulation_history(:,1,100))
-xlabel('Radial position [m]')
+plot(cp_mu,circulation_history(:,1,1),cp_mu,circulation_history(:,1,3),cp_mu,circulation_history(:,1,5),cp_mu,circulation_history(:,1,7),cp_mu,circulation_history(:,1,10),cp_mu,circulation_history(:,1,14),cp_mu,circulation_history(:,1,18),cp_mu,circulation_history(:,1,22),cp_mu,circulation_history(:,1,26),cp_mu,circulation_history(:,1,30),cp_mu,circulation_history(:,1,35),cp_mu,circulation_history(:,1,40),cp_mu,circulation_history(:,1,45),cp_mu,circulation_history(:,1,50),cp_mu,circulation_history(:,1,60),cp_mu,circulation_history(:,1,70),cp_mu,circulation_history(:,1,80),cp_mu,circulation_history(:,1,90));%,cp_mu,circulation_history(:,1,100))
+grid minor
+xlabel('$\frac{r}{R} [-]$','Interpreter','LaTex')
 ylabel('Circulation [m2/s]')
 
 figure(2)
-grid on
-plot(cp_spanwise,circulation_history(:,1,timestep))
-xlabel('Radial position [m]')
+plot(cp_mu,circulation_history(:,1,timestep))
+grid minor
+xlabel('$\frac{r}{R} [-]$','Interpreter','LaTex')
 ylabel('Circulation [m2/s]')
 
 figure(3)
-grid on
-plot(cp_spanwise,C_N)
-xlabel('Radial position [m]')
+plot(cp_mu,C_N)
+grid minor
+xlabel('$\frac{r}{R} [-]$','Interpreter','LaTex')
 ylabel('Normal force coefficient C_N')
 
 figure(4)
-grid on
-plot(cp_spanwise,3*C_T)
-xlabel('Radial position [m]')
+plot(cp_mu,3*C_T)
+grid minor
+xlabel('$\frac{r}{R} [-]$','Interpreter','LaTex')
 ylabel('Thrust force coefficient C_T')
 
 figure(5)
-grid on
-plot(cp_spanwise,alpha_i)
-xlabel('Radial position [m]')
+plot(cp_mu,alpha_i)
+grid minor
+xlabel('$\frac{r}{R} [-]$','Interpreter','LaTex')
 ylabel('Induced angle of attack [deg]')
 
 figure(6)
-grid on
-plot(cp_spanwise,C_L)
-xlabel('Radial position [m]')
+plot(cp_mu,C_L)
+grid minor
+xlabel('$\frac{r}{R} [-]$','Interpreter','LaTex')
 ylabel('Lift coefficient C_L [-]')
 
 figure(7)
-grid on
-plot(cp_spanwise,C_D_i)
-xlabel('Radial position [m]')
+plot(cp_mu,C_D_i)
+grid minor
+xlabel('$\frac{r}{R} [-]$','Interpreter','LaTex')
 ylabel('Induced drag coefficient C_D_i [-]')
 
 figure(8)
-grid on
-plot(cp_spanwise,w_ind)
-xlabel('Radial position [m]')
+plot(cp_mu,w_ind)
+grid minor
+xlabel('$\frac{r}{R} [-]$','Interpreter','LaTex')
 ylabel('Induced velocity in z-direction [m/s]')
+
+figure(9)
+subplot(2,1,1)
+plot(cp_mu,dQ)
+grid minor
+xlabel('$\frac{r}{R} [-]$','Interpreter','LaTex')
+ylabel('Torque [N*m]')
+subplot(2,1,2)
+plot(cp_mu,C_Q)
+grid minor
+xlabel('$\frac{r}{R} [-]$','Interpreter','LaTex')
+ylabel('Torque coefficient [-]')
+
+figure(10)
+plot(linspace(1,max_timestep),Thrust_convergence)
+grid minor
+xlabel('Iteration number [-]')
+ylabel('Thrust [N]')
